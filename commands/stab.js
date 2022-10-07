b793c836335c2f3062385358614316437e08f8c9
arifire21 <ariwolfpup@icloud.com>
2022-10-06 20:17:45 -0400

    initial commit and first command

---
+++ commands/stab.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder, Guild } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('stab')
+		.setDescription('for legal reasons this is a joke (basic cmd)')
+        .addUserOption(option => 
+            option.setName('target')
+                .setDescription('User to stab')
+                .setRequired(true)
+        ),
+	async execute(interaction) {
+		await interaction.reply(`${interaction.user.username} stabbed ${interaction.options.getUser('target')}`);
+	},
+};
---
+++ index.js
@@ -0,0 +1,44 @@
+// Require the necessary discord.js classes
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const { token, ownerId } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+    console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered ${interaction.commandName} interaction.`);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: `@<${ownerId}> There was an error while executing this command!`});
+	}
+});
+
+// When the client is ready, run this code (only once)
+client.once('ready', c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Login to Discord with your client's token
+client.login(token);
