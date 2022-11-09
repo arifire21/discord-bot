87ee036ca084d45c78afcbe14669d1a14d0d7c4a
arifire21 <ariwolfpup@icloud.com>
2022-11-08 22:18:22 -0500

    command returns url via Promises

--- commands/api_test.js
+++ commands/api_test.js
@@ -2,6 +2,15 @@ const axios = require('axios');
 const wait = require('node:timers/promises').setTimeout;
 const { SlashCommandBuilder } = require('discord.js');
 
+//     /*
+//         get_info.js
+
+//         MediaWiki API Demos
+//         Demo of `Info` module: Send a GET request to display information about a page.
+
+//         MIT License
+//     */
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('api-test')
@@ -13,59 +22,46 @@ module.exports = {
         ),
 	async execute(interaction) {
         await interaction.deferReply();
-		await wait(4000);
-        console.log(interaction.options.getString('character'))
-        var resulturl = getCharacterPage(interaction.options.getString('character'))
+		await wait(2000);
+        console.log("submitted: " + interaction.options.getString('character'))
+        var resulturl = await getCharacterPage(interaction.options.getString('character'))
         console.log("result: " + resulturl)
         //todo, if name is empty "char not found"
-	    await interaction.followUp(resulturl);
+	    await interaction.editReply(resulturl || "Character not found! (check spelling)");
 	},
 };
 
-function getCharacterPage(userString){
-    var returnStr = "";
-    /*
-        get_info.js
+function getCharacterPage(userString) {
+    return new Promise((resolve, reject) => {
+        var returnStr = "";
+        var url = "https://rekindled-embers.fandom.com/api.php";
+        var params = {
+            action: "query",
+            format: "json",
+            titles: userString,
+            prop: "info",
+            inprop: "url"
+        };
 
-        MediaWiki API Demos
-        Demo of `Info` module: Send a GET request to display information about a page.
+        url = url + "?origin=*";
+        Object.keys(params).forEach(function (key) { url += "&" + key + "=" + params[key]; });
+        
+        axios.get(url)
+            .then(function (response) {
+                var pages = response.data.query.pages;
+                for (var p in pages) {
+                    console.log("Page " + pages[p] + " response:");
+                    console.log(pages[p].canonicalurl);
+                    returnStr = pages[p].canonicalurl;
+                    console.log("assigned: " + returnStr);
+                    resolve(returnStr)
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+                returnStr = "CanonicalURL not found! (error logged)"
+                reject(returnStr)
+            })
 
-        MIT License
-    */
-
-    var url = "https://rekindled-embers.fandom.com/api.php"; 
-
-    var params = {
-        action: "query",
-        format: "json",
-        titles: userString,
-        prop: "info",
-        inprop: "url"
-    };
-
-    url = url + "?origin=*";
-    Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
-
-    //aphend indexpageids to get the id, to access the object
-    // url = url + "&indexpageids";
-
-    axios.get(url)
-        .then(function (response) {
-            var pages = response.data.query.pages;
-            for (var p in pages) {
-                console.log("Page " + pages[p] + " response:");
-                console.log(pages[p].canonicalurl);
-                returnStr = pages[p].canonicalurl;
-                console.log("assigned: " + returnStr);
-            }
-        })
-        .catch(function (error) {
-            console.log(error);
-            returnStr = "Character not found! (error logged)"
-        })
-        // .finally(function () {
-        //     // always executed
-        // });
-
-    return returnStr;
+    })
 }
