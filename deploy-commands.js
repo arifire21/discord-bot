49252aa59e7ed2fc6b91da7692d7dcd1a476d3f4
arifire21 <ariwolfpup@icloud.com>
2023-09-21 07:00:41 -0400

    change to global deployment

--- deploy-commands.js
+++ deploy-commands.js
@@ -25,14 +25,14 @@ const rest = new REST({ version: '10' }).setToken(token);
 
 (async () => {
 	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+		console.log(`[GLOBAL] Started refreshing ${commands.length} application (/) commands.`);
 
 		const data = await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
+			Routes.applicationCommands(clientId),
 			{ body: commands },
 		);
 
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+		console.log(`[GLOBAL] Successfully reloaded ${data.length} application (/) commands.`);
 	} catch (error) {
 		console.error(error);
 	}
